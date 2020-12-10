b0725519d552c403ee55e7cbc2f1cf4572bd17cf
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 13:15:34 +1300

    Added comments
    Added comments to help beginners further understand aspects of this script

--- Minecraft coordinates.py
+++ Minecraft coordinates.py
@@ -1,10 +1,16 @@
+#import  functions
 from mcpi.minecraft import Minecraft
 from turtle import *
 import time
+
+#define variables
 mc = Minecraft.create()
 style = ('Courier', 30, 'italic')
+
+#mainloop
 while True:
     pos = mc.player.getPos()
     write(pos, font=style, align='center')
     time.sleep(0.2)
     clear()
+    
