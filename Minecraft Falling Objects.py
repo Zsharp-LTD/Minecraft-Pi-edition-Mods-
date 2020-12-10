f16789b0f653f65d008f635738a5888e16797b8e
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 13:09:04 +1300

    Added comments
    Added comments to help beginners

--- Minecraft Falling Objects.py
+++ Minecraft Falling Objects.py
@@ -1,7 +1,12 @@
+#import functions
 from mcpi.minecraft import Minecraft
 from time import sleep
+
+#define variables
 mc = Minecraft.create()
 flower = 38
+
+#mainloop
 while True:
     x, y, z = mc.player.getPos()
     mc.setBlock(x, y, z, flower)
