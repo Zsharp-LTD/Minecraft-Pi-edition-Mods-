ca8df9c8af4b647e4d6633db9a174eb49564db2b
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 13:22:41 +1300

    Added comments
    Added comments to help beginners further understand aspects of this script

--- TNT_TRAIL_SMALL.py
+++ TNT_TRAIL_SMALL.py
@@ -1,6 +1,11 @@
+#import and rename functions
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
+
+#define variables
 tnt = 46
+
+#mainloop
 while True:
     x, y, z = mc.player.getPos()
     mc.setBlock(x, y, z, tnt, 1)
