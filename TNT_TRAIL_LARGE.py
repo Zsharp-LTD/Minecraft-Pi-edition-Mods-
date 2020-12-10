61358c4edf6841b4e65b8966c4ea7bb0b5f684d2
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 13:20:45 +1300

    Added comments
    Added comments to help beginners further understand this script

--- TNT_TRAIL_LARGE.py
+++ TNT_TRAIL_LARGE.py
@@ -1,6 +1,11 @@
+#import functions
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
+
+#define variable
 tnt = 46
+
+#mainloop
 while True:
     x, y, z, = mc.player.getPos()
     mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, tnt, 1)
