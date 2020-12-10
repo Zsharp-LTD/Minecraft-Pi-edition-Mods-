52352a64686e9cd4502b37e5ee15e848aa029255
hackercyclops12 <63036550+hackercyclops12@users.noreply.github.com>
2020-12-10 13:04:58 +1300

    Update Lava Trail.py

--- Lava Trail.py
+++ Lava Trail.py
@@ -1,6 +1,11 @@
+#import functions
 from mcpi.minecraft import Minecraft
+
+#define variables
 mc = Minecraft.create()
 LAVA = 10
+
+#mainloop
 while True:
     x, y, z = mc.player.getPos()
     mc.setBlock(x, y, z, LAVA, 1)
